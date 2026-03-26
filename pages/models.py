c6cef0468d6d75c364b8307c69ee33221d5ca583
Simkagame <131748949+SimkaGame@users.noreply.github.com>
2026-03-26 13:57:27 +0300

    Ready Task 5

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ProjectGoal(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название цели")
+    
+    description = models.TextField(verbose_name="Описание")
+    
+    priority = models.IntegerField(default=1, verbose_name="Приоритет")
+    
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.name
