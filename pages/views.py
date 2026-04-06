61c571b19e8701edcabb4188b63f15470635ff31
Simkagame <131748949+SimkaGame@users.noreply.github.com>
2026-04-06 12:35:51 +0300

    Fix path html files

--- pages/views.py
+++ pages/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render
 from .models import ProjectGoal
 
 def index(request):
-    goals = ProjectGoal.objects.all()
-    
     context = {
         'title': 'PROgress – главная страница',
-        'welcome_text': 'Сервис для отслеживания задач и личного развития',
-        'goals': goals,
+        'welcome_text': 'Сервис для отслеживания  игровых задач',
     }
     return render(request, 'index.html', context)
 
+def daily(request):
+    goals = ProjectGoal.objects.all() 
+    context = {
+        'goals': goals,
+    }
+    return render(request, 'daily.html', context)
+
 def about(request):
     return render(request, 'about.html')
-
-def daily(request):
-    return render(request, 'daily.html')
 
 def progress(request):
     return render(request, 'progress.html')
