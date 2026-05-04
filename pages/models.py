39568cb7c18245c439f529fcd46e0d175f2c0893
Simkagame <131748949+SimkaGame@users.noreply.github.com>
2026-05-04 09:56:59 +0300

    Auth.users and login

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class ProjectGoal(models.Model):
     name = models.CharField(max_length=200, verbose_name="Название цели")
@@ -8,6 +9,8 @@ class ProjectGoal(models.Model):
     priority = models.IntegerField(default=1, verbose_name="Приоритет")
     
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
 
     def __str__(self):
         return self.name
