d5d9f3d8bb855283b94802dcc4f6dacbdaf11d98
Simkagame <131748949+SimkaGame@users.noreply.github.com>
2026-05-07 21:39:47 +0300

    Opportunity adding image for carts

--- pages/models.py
+++ pages/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название тега")
+
+    def __str__(self):
+        return self.name
+
 class ProjectGoal(models.Model):
     name = models.CharField(max_length=200, verbose_name="Название цели")
-    
     description = models.TextField(verbose_name="Описание")
-    
     priority = models.IntegerField(default=1, verbose_name="Приоритет")
-    
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    image = models.ImageField(upload_to='goals/', blank=True, null=True, verbose_name="Изображение")
+    tags = models.ManyToManyField(Tag, blank=True, related_name='goals', verbose_name="Теги")
 
     def __str__(self):
         return self.name
